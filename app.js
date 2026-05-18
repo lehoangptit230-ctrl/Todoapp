33a0a669bdddf5faff22e8ece8f43b8f555b449f
lehoangptit230-ctrl <lehoangptit230@gmail.com>
2026-05-18 12:15:59 +0700

    sua code app

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const todoList = document.getElementById('todo-list');
 
 addBtn.addEventListener('click', () => {
     if (input.value.trim() !== "") {
-            const li = document.createElement('li');
-                    li.innerHTML = `
-                                <span>${input.value}</span>
-                                            <button class="delete-btn">X</button>
-                                                    `;
-                                                            todoList.appendChild(li);
-                                                                    [span_3](start_span)input.value = ""; 
-                                                                        }
-                                                                        });
-                                                                        
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${input.value}</span>
+            <button class="delete-btn">X</button>
+        `;
+        todoList.appendChild(li);
+        input.value = ""; // Xóa nội dung input sau khi thêm 
+    }
+});
